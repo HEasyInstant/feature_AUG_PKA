4313c996a762b1a470da5a52501682a6ce4efddf
PIYUSH KUMAR ANAND <39540002+HEasyInstant@users.noreply.github.com>
2022-04-10 01:58:12 +0530

    Update HeasyinstantApplicationTests.java
    master commit

--- heasyinstant/src/test/java/com/dev/heasyinstant/HeasyinstantApplicationTests.java
+++ heasyinstant/src/test/java/com/dev/heasyinstant/HeasyinstantApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dev.heasyinstant;
+/*package com.dev.heasyinstant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class HeasyinstantApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
